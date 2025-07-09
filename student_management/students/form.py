86d64c8c457b1621044bb2b71e2ec971153eb0c5
TKW-TrinhQuocDat <trinhquocdat041004@gmail.com>
2025-07-10 00:43:12 +0700

    Hoan thanh quan ly lop hoc

--- student_management/students/form.py
+++ student_management/students/form.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import StudentInfo, ParentInfo
+from .models import StudentInfo, ParentInfo, Classroom
 
 class StudentForm(ModelForm):
     class Meta:
@@ -14,3 +14,21 @@ class ParentForm(ModelForm):
     class Meta:
         model  = ParentInfo
         fields = ("name", "phone", "email")
+
+
+
+class ClassroomForm(forms.ModelForm):
+    class Meta:
+        model = Classroom
+        fields = ["classroom_name", "grade"]
+
+
+class AddStudentForm(forms.Form):
+    student = forms.ModelChoiceField(queryset=StudentInfo.objects.filter(status=True))
+    transfer_date = forms.DateField(required=False, widget=forms.DateInput(attrs={"type": "date"}))
+
+
+class TransferStudentForm(forms.Form):
+    student = forms.ModelChoiceField(queryset=StudentInfo.objects.filter(status=True))
+    new_classroom = forms.ModelChoiceField(queryset=Classroom.objects.all())
+    transfer_date = forms.DateField(required=False, widget=forms.DateInput(attrs={"type": "date"}))
--- student_management/students/templatetags/form_tags.py
+++ student_management/students/templatetags/form_tags.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css):
     return field.as_widget(attrs={"class": css})
+
+
+@register.filter
+def get_item(dict_data, key):
+    return dict_data.get(key)
